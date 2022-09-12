68833c603cecad307a0cc9ecfb6bd626e2d0f41f
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-12 11:58:18 +0100

    list used instead of string

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -2,9 +2,34 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        private static bool IsDivisibleBy(int numerator, int denominator)
+        {
+            return numerator % denominator == 0;
+        }
         public string FizzBuzzFor(int n)
         {
-            return n.ToString();
+            List<String> result = new List<string>();
+
+            if (IsDivisibleBy(n, 3))
+            {
+                result.Add("Fizz");
+            }
+
+            if (IsDivisibleBy(n, 5))
+            {
+                result.Add("Buzz");
+            }
+
+            string resultString = String.Join("", result);
+
+            if (!result.Any())
+            {
+                return n.ToString();
+            }
+            else
+            {
+                return resultString;
+            }
         }
     }
 }
